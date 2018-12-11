f94da7dab3df49033a42badfc330d6bd6f45c1c8
Manoj <manojkumarmc@hotmail.com>
2018-12-11 14:50:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
-import os
+import hug
 
-os.execl('uwsgi --http 0.0.0.0:8080 --wsgi-file server.py --callable __hug_wsgi__')
+#os.execl('uwsgi --http 0.0.0.0:8080 --wsgi-file server.py --callable __hug_wsgi__')
+
+hug.API(__name__).http.serve(port=8080)
